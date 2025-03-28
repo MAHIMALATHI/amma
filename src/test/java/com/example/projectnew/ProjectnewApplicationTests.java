2bf35eec03f14af1bf197b2e229a030c6de37b5e
MAHIMALATHI <144418489+MAHIMALATHI@users.noreply.github.com>
2025-03-28 10:33:01 +0530

    Add files via upload

---
+++ src/test/java/com/example/projectnew/ProjectnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
